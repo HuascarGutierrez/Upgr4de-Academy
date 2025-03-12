793ee3e16089f7815a6eb4adaaf4f4dca6d6cd99
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-03-12 02:36:49 -0400

    merge ikon

--- src/pages/SAPIMain.jsx
+++ src/pages/SAPIMain.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavLateral from '../components/organisms/NavLateral'
 import './styles/SAPIMain.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Perfil from '../components/organisms/Perfil'
 import SignUp from './SignUp'
 import Login from './Login'
 import Supervision from '../components/organisms/Supervision'
@@ -17,9 +18,10 @@ function SAPIMain() {
                  * <Route path='hola' element={<SignUp/>}/>
                   <Route path='adios' element={<Login/>}/>
                  */
-                  <Route path='catalogo' element={<CoursesCatalog/>}/>
-                }
+              }
+                <Route path='catalogo' element={<CoursesCatalog/>}/>
                 <Route path='supervision' element={<Supervision/>}/>
+                <Route path='/Perfil' element={<Perfil/>}/>
             </Routes>
         </div>
     </div>

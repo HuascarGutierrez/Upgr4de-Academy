de269bbaaa1f774558aca71e3f116c2f27b5a326
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-03-22 09:38:22 -0400

    xd

--- src/pages/SAPIMain.jsx
+++ src/pages/SAPIMain.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignUp from './SignUp'
 import Login from './Login'
 import Courses from '../components/organisms/Courses'
-import CoursesCatalog from '../components/organisms/coursesCatalog'
 import Perfil from '../components/organisms/Perfil'
 import Supervision from '../components/organisms/Supervision'
+import CoursesCatalog from '../components/organisms/CoursesCatalog'
 import CourseDetail from '../components/organisms/CourseDetail'
 
 function SAPIMain({user}) {

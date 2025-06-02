fb5e72e001d531dec42ca6291a4a5a47c14327b4
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-06-02 10:59:40 -0400

    mmda del ivan

---
+++ src/components/organisms/VisualizarEvaluaciones.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ModuloEjercicios from './ModuloEjercicios'
+
+function VisualizarEvaluaciones() {
+  return (
+    <div>
+        <ModuloEjercicios/>
+    </div>
+  )
+}
+
+export default VisualizarEvaluaciones
